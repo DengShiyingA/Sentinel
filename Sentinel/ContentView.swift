1a9e5983344f43cecd8cb940c989b9e443f4181a
two <3281341052@qq.com>
2026-04-08 12:20:46 +0800

    Initial Commit

---
+++ Sentinel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sentinel
+//
+//  Created by 邓诗颖 on 2026/4/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sentinel/SentinelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentinelApp.swift
+//  Sentinel
+//
+//  Created by 邓诗颖 on 2026/4/8.
+//
+
+import SwiftUI
+
+@main
+struct SentinelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
